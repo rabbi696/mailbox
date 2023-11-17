f3166b3364e4f6e95039be2c2a8ea628a1a92986
Golam Rabbi <52798464+rabbi696@users.noreply.github.com>
2023-11-18 04:19:21 +0600

    Update mailbox.php

--- mailbox.php
+++ mailbox.php
@@ -2,3 +2,19 @@ namespace Mailbox\Config;
 
 use CodeIgniter\Config\BaseConfig;
 use Mailbox\Models\Mailbox_settings_model;
+class Mailbox extends BaseConfig {
+
+    public $app_settings_array = array(
+        "mailbox_email_file_path" => PLUGIN_URL_PATH. "Mailbox/files/email_files/"
+    );
+
+    public function __construct() {
+        $mailbox_settings_model = new Mailbox_settings_model();
+
+        $settings = $mailbox_settings_model->get_all_settings()->getResult();
+        foreach ($settings as $setting) {
+            $this->app_settings_array[$setting->setting_name] = $setting->setting_value;
+        }
+    }
+
+}
